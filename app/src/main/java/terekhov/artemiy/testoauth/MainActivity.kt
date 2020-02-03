69599bef817b01496d49a1542e2ad7d5767b6056
Artemiy Terekhov <artemiy.terekhov@gmail.com>
2020-02-03 14:37:38 +0300

    Initial commit

---
+++ app/src/main/java/terekhov/artemiy/testoauth/MainActivity.kt
@@ -0,0 +1,19 @@
+package terekhov.artemiy.testoauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import terekhov.artemiy.testoauth.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
